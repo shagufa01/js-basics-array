944bb1ac21788540940f98666a9e17759e48f205
shagufa01 <shagufanaaz321@gmail.com>
2020-03-02 19:45:06 +0530

    array

---
+++ script.js
@@ -0,0 +1,102 @@
+
+var a = 10;
+ if (a<0) {
+      console.log('a is negative');
+    }else if (a>0) {
+       console.log('a is positive');
+    }else {
+        console.log('a is zero');
+    } 
+  
+
+
+
+function sub(c,d) {
+     var e = c-d;
+   return e;
+}
+console.log(sub(10,4));
+
+function add(a,b) {
+    var c = a+b;
+    return c;
+}
+console.log(add(6,4));
+
+for (i = 0; i < 8; i++) {
+    console.log(i);
+}
+
+var a = 0;
+while ( a < 5) {
+    a++;
+    console.log(a);
+    
+}
+var n=10;
+for(a=0;a<n;a++){
+    console.log(a);
+    
+}
+
+
+
+
+var c;
+function division(a,b){
+    var c=a/b;
+    return c;
+    
+    
+}
+c=division(10,5)
+console.log(c);
+
+
+
+
+var x=0;
+if(x<0){
+    console.log("x is negative");
+    
+}else if(x==0){
+    console.log("x is zero");
+    
+}else{
+    console.log("x is positive");
+    
+    
+}
+
+
+
+var animal='dog';
+
+switch(animal){
+
+    case 'cat':
+        console.log("meoww");
+        break;
+    case 'dog':
+        console.log("bark");
+        break;
+    default:
+        console.log("humans");
+        
+        
+}
+var array=["good","morning","evening","noon"]
+console.log(array.length);
+
+//array
+
+var array = [1,2,3,4,5];
+console.log(array = [1,2,3,4,5]);
+function integers() {
+    for (i=0;i<=array;i++)
+    console.log(integers);
+    }
+
+
+
+
